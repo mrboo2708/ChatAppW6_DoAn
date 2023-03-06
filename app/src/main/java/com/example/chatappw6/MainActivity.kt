1025b93955fb462bbb8ff700b22934d6fa36db94
Thanh Luan Nguyen <mrboo2707@gmail.com>
2023-03-06 15:05:54 +0700

    complete project

---
+++ app/src/main/java/com/example/chatappw6/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.chatappw6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
